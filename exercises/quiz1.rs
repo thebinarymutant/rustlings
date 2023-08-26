67ca6a301e7d93cd00fe447670bd806973afb88b
Smit Patel <the1995captainh@gmail.com>
2023-08-27 00:40:51 +0530

    5 quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: u64) -> u64 {
+    match quantity > 40 {
+        true => quantity,
+        false => quantity * 2,
+    }
+}
 
 // Don't modify this function!
 #[test]
