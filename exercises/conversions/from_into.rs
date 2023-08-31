fbbaaaa051f3f0d6184cfe5c7915d2f8f39ff655
Smit Patel <the1995captainh@gmail.com>
2023-08-31 15:59:45 +0530

    27 conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,32 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let mut split = s.split(",");
+
+        let n = split.next();
+        let a = split.next();
+
+        if let Some(name) = n {
+            if name.len() != 0 {
+                if let Some(age_str) = a {
+                    if let Ok(age) = age_str.parse::<usize>() {
+                        return Person {
+                            name: name.to_owned(),
+                            age,
+                        };
+                    }
+                }
+            }
+        }
+
+        Person::default()
+
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +35,19 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+fn valid_rgb(col: i16) -> Result<u8, IntoColorError> {
+    u8::try_from(col).map_err(|_| IntoColorError::IntConversion)
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let red = valid_rgb(tuple.0)?;
+        let green = valid_rgb(tuple.1)?;
+        let blue = valid_rgb(tuple.2)?;
+
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -48,6 +55,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let red = valid_rgb(arr[0])?;
+        let green = valid_rgb(arr[1])?;
+        let blue = valid_rgb(arr[2])?;
+
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -55,6 +67,15 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+                if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        let red = valid_rgb(slice[0])?;
+        let green = valid_rgb(slice[1])?;
+        let blue = valid_rgb(slice[2])?;
+
+        Ok(Color { red, green, blue })
     }
 }
 
