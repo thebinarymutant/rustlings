8ea8120fd71c70e54053760b03ad4f6c90169dfa
Smit <smitp.contact@gmail.com>
2024-12-18 05:22:38 +0530

    Savepoint enums

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -7,6 +7,11 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Quit,
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Resize { width: u64, height: u64 },
 }
 
 impl Message {
--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Quit => self.quit(),
+            Message::Move(position) => self.move_position(position),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor(r, g, b) => self.change_color(r, g, b),
+            Message::Resize { width, height } => self.resize(width, height),
+        }
     }
 }
 
